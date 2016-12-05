3d21f4e1897cb648ac5eab433c86b57e2cb120a8
NishantUpadhyay-BTC <nishantupadhyay@botreeconsulting.com>
2016-12-05 17:40:25 +0530

    remove before filter

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,6 +1,7 @@
 class HooksController < ApplicationController
   skip_before_filter :verify_authenticity_token
-  before_filter :check_hooks_access
+  # before_filter :check_hooks_access
+
   def check_hooks_access
     if request.headers['X-Hooks-Auth-Header'] != 'superSecretAuthToken'
       render :file => 'public/401.html', :status => :unauthorized, :layout => false
