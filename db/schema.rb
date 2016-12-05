99a493b374bae9e69c2eeb1266e0a2c23805f712
NishantUpadhyay-BTC <nishantupadhyay@botreeconsulting.com>
2016-12-05 10:59:49 +0530

    Add Store model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161205044611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stores", force: :cascade do |t|
+    t.string   "store_hash"
+    t.string   "access_token"
+    t.text     "scope"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
