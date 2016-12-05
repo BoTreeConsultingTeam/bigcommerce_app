831ffa61ae6216d2f873f0ff67795a06627a7eab
NishantUpadhyay-BTC <nishantupadhyay@botreeconsulting.com>
2016-12-05 15:51:31 +0530

    add loggers

--- app/controllers/omniauths_controller.rb
+++ app/controllers/omniauths_controller.rb
@@ -21,10 +21,13 @@ class OmniauthsController < ApplicationController
       # Create store record
       logger.info "[install] Installing app for store '#{store_hash}' with admin '#{email}'"
       store = Store.create(store_hash: store_hash, access_token: token, scope: scope, user_email: email, username: name)
-      if store
+      if store.present?
         connection = Bigcommerce::Connection.build(Bigcommerce::Config.new(store_hash: store.store_hash, client_id: ENV['BC_CLIENT_ID'], access_token: store.access_token))
-        Bigcommerce::Webhook.create( scope: 'store/order/created',  destination: "#{ENV[APP_URL]}order_created",  connection: connection  )
-        Bigcommerce::Webhook.create( scope: 'store/shipment/created',  destination: "#{ENV[APP_URL]}shipment_created",  connection: connection  )
+        puts "Bigcommerce::System.time(connection: connection) >>>>>>> #{Bigcommerce::System.time(connection: connection)}"
+        webhook1 = Bigcommerce::Webhook.create!( scope: 'store/order/created',  destination: "#{ENV[APP_URL]}order_created",  connection: connection  )
+        puts "webhook1 >>>>>>> #{webhook1}"
+        webhook2 = Bigcommerce::Webhook.create!( scope: 'store/shipment/created',  destination: "#{ENV[APP_URL]}shipment_created",  connection: connection  )
+        puts "webhook2 >>>>>>> #{webhook2}"
       end
       session[:store_id] = store.id
     end
