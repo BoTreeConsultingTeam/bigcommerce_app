3c365a02aaf029a448720337b187646a2b6b3553
NishantUpadhyay-BTC <nishantupadhyay@botreeconsulting.com>
2016-12-07 19:06:20 +0530

    refactor default template

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -4,8 +4,6 @@ class HooksController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def order_created
-    puts "#{params} PARAMS"
-
     order_id = params['data']['id']
     store_hash = params['producer'].split('/').last
     store = Store.find_by(store_hash: store_hash)
@@ -23,14 +21,10 @@ class HooksController < ApplicationController
       redirect_to root_path and return
     end
 
-    puts "========================ORDER DETAILS=============================="
-    puts "#{@order}"
-    puts "======================================================"
-
     order_products = Bigcommerce::OrderProduct.all(order_id, connection: connection)
 
-    # Bigcommerce::ProductImage.all(112, connection: connection) ccan be used to get product images
     @order_products = []
+
     if order_products
       order_products.each do |product|
         @order_products << OpenStruct.new(product)
@@ -39,19 +33,9 @@ class HooksController < ApplicationController
       redirect_to root_path and return
     end
 
-    puts "======================PRODUCT DETAILS================================"
-    puts "#{@order_products}"
-
     @customer = Bigcommerce::Customer.find(@order.customer_id, connection: connection)
-
-    puts "======================CUSTOMER DETAILS================================"
-    puts "#{@customer}"
-
     shipping_addresses = Bigcommerce::OrderShippingAddress.all(@order.id, connection: connection)
-
     @shiping_address = OpenStruct.new(shipping_addresses.first)
-    puts "======================Shipping Address  DETAILS================================"
-    puts "#{@shiping_address}"
 
     var_hash = prepare_variable_hash(@order, @order_products, @customer, @shiping_address,connection)
     Mail.defaults do
@@ -110,7 +94,7 @@ class HooksController < ApplicationController
     product_template_str = <<-START
       <tr>
        <td style='width:150px;text-align:center;padding:16px 0 10px 0;vertical-align:top;font-size:13px;line-height:18px;font-family:Arial,sans-serif' class='m_-8458387270647573780m_-8456204030309822964m_4722856042003582080m_-5527024756179962845photo'> <a data-saferedirecturl='https://www.google.com/url?hl=en&amp;q=https://www.amazon.in/gp/r.html?C%3D1H6X0AT2K3AR2%26K%3DAC1H0DB3Q6BF%26R%3D1NXICTSMU7RGK%26T%3DC%26U%3Dhttp%253A%252F%252Fwww.amazon.in%252Fdp%252FB00KKLVY22%252Fref%253Dpe_386221_48200641_TE_item_image%26A%3D8UWEP2UPJ8VFS574JIXZ6MIUMGYA%26H%3DAVMNHANDIJWU4XMFNAJ28PAA9T8A%26ref_%3Dpe_386221_48200641_TE_item_image&amp;source=gmail&amp;ust=1481170866938000&amp;usg=AFQjCNFUNU4Qv2LwAuRcJWwEw_snaS8KBw' target='_blank' style='text-decoration:none;color:rgb(0,102,153);font:12px/16px Arial,sans-serif' title='B00KKLVY22' href='https://www.amazon.in/gp/r.html?C=1H6X0AT2K3AR2&amp;K=AC1H0DB3Q6BF&amp;R=1NXICTSMU7RGK&amp;T=C&amp;U=http%3A%2F%2Fwww.amazon.in%2Fdp%2FB00KKLVY22%2Fref%3Dpe_386221_48200641_TE_item_image&amp;A=8UWEP2UPJ8VFS574JIXZ6MIUMGYA&amp;H=AVMNHANDIJWU4XMFNAJ28PAA9T8A&amp;ref_=pe_386221_48200641_TE_item_image'> <img style='border:0' src='%%PRODUCT_image_source%%' class='CToWUd'> </a> </td>
-       <td style='color:rgb(102,102,102);padding:10px 0 0 0;vertical-align:top;font-size:13px;line-height:18px;font-family:Arial,sans-serif' class='m_-8458387270647573780m_-8456204030309822964m_4722856042003582080m_-5527024756179962845name'> <a data-saferedirecturl='https://www.google.com/url?hl=en&amp;q=https://www.amazon.in/gp/r.html?C%3D1H6X0AT2K3AR2%26K%3DAC1H0DB3Q6BF%26R%3D1NXICTSMU7RGK%26T%3DC%26U%3Dhttp%253A%252F%252Fwww.amazon.in%252Fdp%252FB00KKLVY22%252Fref%253Dpe_386221_48200641_TE_item%26A%3DQAZVHKQZEUDP2EUYEF6VNAYSEE0A%26H%3DNZAAHUYVBNDH7H17H7Q7XAZZX9WA%26ref_%3Dpe_386221_48200641_TE_item&amp;source=gmail&amp;ust=1481170866938000&amp;usg=AFQjCNHqy3_6mbzV6tyFmlhRKdoVyHDAtQ' target='_blank' style='text-decoration:none;color:rgb(0,102,153);font:12px/16px Arial,sans-serif' href='https://www.amazon.in/gp/r.html?C=1H6X0AT2K3AR2&amp;K=AC1H0DB3Q6BF&amp;R=1NXICTSMU7RGK&amp;T=C&amp;U=http%3A%2F%2Fwww.amazon.in%2Fdp%2FB00KKLVY22%2Fref%3Dpe_386221_48200641_TE_item&amp;A=QAZVHKQZEUDP2EUYEF6VNAYSEE0A&amp;H=NZAAHUYVBNDH7H17H7Q7XAZZX9WA&amp;ref_=pe_386221_48200641_TE_item'> <b>%%PRODUCT_quantity%% x </b>%%PRODUCT_name%% </a>
+       <td style='color:rgb(102,102,102);padding:10px 10 0 0;vertical-align:top;font-size:13px;line-height:18px;font-family:Arial,sans-serif' class='m_-8458387270647573780m_-8456204030309822964m_4722856042003582080m_-5527024756179962845name'> <a data-saferedirecturl='https://www.google.com/url?hl=en&amp;q=https://www.amazon.in/gp/r.html?C%3D1H6X0AT2K3AR2%26K%3DAC1H0DB3Q6BF%26R%3D1NXICTSMU7RGK%26T%3DC%26U%3Dhttp%253A%252F%252Fwww.amazon.in%252Fdp%252FB00KKLVY22%252Fref%253Dpe_386221_48200641_TE_item%26A%3DQAZVHKQZEUDP2EUYEF6VNAYSEE0A%26H%3DNZAAHUYVBNDH7H17H7Q7XAZZX9WA%26ref_%3Dpe_386221_48200641_TE_item&amp;source=gmail&amp;ust=1481170866938000&amp;usg=AFQjCNHqy3_6mbzV6tyFmlhRKdoVyHDAtQ' target='_blank' style='text-decoration:none;color:rgb(0,102,153);font:12px/16px Arial,sans-serif' href='https://www.amazon.in/gp/r.html?C=1H6X0AT2K3AR2&amp;K=AC1H0DB3Q6BF&amp;R=1NXICTSMU7RGK&amp;T=C&amp;U=http%3A%2F%2Fwww.amazon.in%2Fdp%2FB00KKLVY22%2Fref%3Dpe_386221_48200641_TE_item&amp;A=QAZVHKQZEUDP2EUYEF6VNAYSEE0A&amp;H=NZAAHUYVBNDH7H17H7Q7XAZZX9WA&amp;ref_=pe_386221_48200641_TE_item'> <b>%%PRODUCT_quantity%% x </b>%%PRODUCT_name%% </a>
        </td>
        <td style='width:110px;text-align:right;font-size:14px;padding:10px 10px 0 0;vertical-align:top;line-height:18px;font-family:Arial,sans-serif' class='m_-8458387270647573780m_-8456204030309822964m_4722856042003582080m_-5527024756179962845price'> <strong> %%PRODUCT_price%% </strong> <br> </td>
       </tr>
